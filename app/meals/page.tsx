4a13d2b45950fdb2ec7db7197179d501905e045c
Pedro Almeida <henrique.pedro.1992@gmail.com>
2024-06-12 17:21:21 -0300

    create meals page

--- app/meals/page.tsx
+++ app/meals/page.tsx
@@ -1,3 +1,26 @@
+import Link from "next/link";
+
+import styles from "./page.module.css";
+import { MealsGrid } from "@/components/meals-grid";
+
 export default function Meals() {
-  return <h1>Meals</h1>;
+  return (
+    <>
+      <header className={styles.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={styles.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun.
+        </p>
+        <p className={styles.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={styles.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
+  );
 }
