57bb4e197746afc832e75429560b8ee61b0323e4
Pedro Almeida <henrique.pedro.1992@gmail.com>
2024-06-12 15:24:39 -0300

    feat: add main header background to header

--- components/main-header/main-header.tsx
+++ components/main-header/main-header.tsx
@@ -3,26 +3,30 @@ import Image from "next/image";
 
 import logoImage from "@/assets/logo.png";
 
+import { MainHeaderBackground } from "./components/main-header-background";
 import styles from "./main-header.module.css";
 
 export function MainHeader() {
   return (
-    <header className={styles.header}>
-      <Link href="/" className={styles.logo}>
-        <Image src={logoImage} alt="A plate with food on it" priority />
-        Next Level Food
-      </Link>
+    <>
+      <MainHeaderBackground />
+      <header className={styles.header}>
+        <Link href="/" className={styles.logo}>
+          <Image src={logoImage} alt="A plate with food on it" priority />
+          Next Level Food
+        </Link>
 
-      <nav className={styles.nav}>
-        <ul>
-          <li>
-            <Link href="/meals">Meals</Link>
-          </li>
-          <li>
-            <Link href="/community">Foodies Community</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+        <nav className={styles.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">Meals</Link>
+            </li>
+            <li>
+              <Link href="/community">Foodies Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
   );
 }
